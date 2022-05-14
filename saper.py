7c09602d724c26f471694b14b576be14dbeca21b
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-05-14 17:00:16 +0300

    Add toolbar

--- saper.py
+++ saper.py
@@ -27,9 +27,49 @@ class MainWindow(QMainWindow):
         """
         super().__init__(*args, **kwargs)
 
+        self.level = 0
+        self.board_size, self.n_mines = LEVELS[self.level]
+
         self.setWindowTitle('Сапер')
         self.setFixedSize(300, 300)
+        self.initUI()
         self.show()
+
+    def initUI(self):
+        """
+        Настройка пользовательского интерфейса
+        """
+        w = QWidget()
+        hb = QHBoxLayout()
+
+        self.mines = QLabel(str(self.n_mines))
+        self.mines.setAlignment(Qt.AlignCenter)
+
+        self.clock = QLabel('000')
+        self.clock.setAlignment(Qt.AlignCenter)
+
+        f = self.mines.font()
+        f.setPointSize(24)
+        f.setWeight(75)
+        self.mines.setFont(f)
+        self.clock.setFont(f)
+
+        self.button = QPushButton()
+        self.button.setFixedSize(32, 32)
+        self.button.setIconSize(QSize(32, 32))
+        self.button.setIcon(QIcon('./images/smiley.png'))
+        self.button.setFlat(True)
+
+        hb.addWidget(self.mines)
+        hb.addWidget(self.button)
+        hb.addWidget(self.clock)
+
+        vb = QVBoxLayout()
+        vb.addLayout(hb)
+
+        w.setLayout(vb)
+        self.setCentralWidget(w)
+
 
 if __name__ == '__main__':
     app = QApplication([])
